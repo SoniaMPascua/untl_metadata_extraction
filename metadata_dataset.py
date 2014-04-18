6c7e01d02d37d4c0598657391e88816ef243a0ec
vphill <vphill@gmail.com>
2014-04-18 07:16:08 -0500

    pep8

--- metadata_dataset.py
+++ metadata_dataset.py
@@ -1,9 +1,8 @@
 import sys
 import hashlib
-import csv
-import fileinput
 from datetime import datetime
 from pyuntl.untldoc import untlxml2py, untlpy2dict
+
 
 def get_is_hidden(untl_dict):
     untl_dict = untl_dict
@@ -13,6 +12,7 @@ def get_is_hidden(untl_dict):
             hidden = meta["content"]
     return hidden
 
+
 def get_metadata_creator(untl_dict):
     untl_dict = untl_dict
     metadata_creator = "None"
@@ -20,6 +20,7 @@ def get_metadata_creator(untl_dict):
         if meta["qualifier"] == "metadataCreator":
             metadata_creator = meta["content"]
     return metadata_creator
+
 
 def get_metadata_editor(untl_dict):
     untl_dict = untl_dict
@@ -29,6 +30,7 @@ def get_metadata_editor(untl_dict):
             metadata_editor = meta["content"]
     return metadata_editor
 
+
 def get_metadata_creation_date(untl_dict):
     untl_dict = untl_dict
     metadata_creation_date = "None"
@@ -37,13 +39,15 @@ def get_metadata_creation_date(untl_dict):
             metadata_creation_date = meta["content"]
     return metadata_creation_date
 
+
 def get_metadata_edit_date(untl_dict):
     untl_dict = untl_dict
     metadata_edit_date = "None"
     for meta in untl_dict["meta"]:
         if meta["qualifier"] == "metadataModificationDate":
             metadata_edit_date = meta["content"]
-    return metadata_edit_date   
+    return metadata_edit_date
+
 
 def get_ark(untl_dict):
     untl_dict = untl_dict
@@ -51,17 +55,14 @@ def get_ark(untl_dict):
     for meta in untl_dict["meta"]:
         if meta["qualifier"] == "ark":
             ark = meta["content"]
-    return ark   
+    return ark
 
 
-
-
-
-metadata_fields = ["title", "creator", "contributor", "publisher", "date", \
-            "language", "description", "subject", "primarySource", "coverage", \
-             "source", "citation", "relation", "collection", "institution", \
-             "rights", "resourceType", "format", "identifier", "degree", "note", \
-            "meta"]
+metadata_fields = ["title", "creator", "contributor", "publisher", "date",
+                   "language", "description", "subject", "primarySource",
+                   "coverage", "source", "citation", "relation", "collection",
+                   "institution", "rights", "resourceType", "format",
+                   "identifier", "degree", "note", "meta"]
 
 header = True
 
@@ -71,7 +72,6 @@ for filename in sys.stdin:
     rd["completeness"] = untl.completeness
     rd["record_length"] = untl.record_length
     rd["record_content_length"] = untl.record_content_length
-
 
     untl_dict = untlpy2dict(untl)
     for field in metadata_fields:
@@ -86,25 +86,21 @@ for filename in sys.stdin:
     rd["metadata_edit_date"] = get_metadata_edit_date(untl_dict)
     rd["ark"] = get_ark(untl_dict)
 
-
-
     if rd["metadata_creation_date"] != "None":
-        creation_time = datetime.strptime(rd["metadata_creation_date"], "%Y-%m-%d, %H:%M:%S")
+        creation_time = datetime.strptime(rd["metadata_creation_date"],
+                                          "%Y-%m-%d, %H:%M:%S")
 
     if rd["metadata_edit_date"] != "None":
         #2014-03-12, 11:42:25
-        edit_time = datetime.strptime(rd["metadata_edit_date"], "%Y-%m-%d, %H:%M:%S")
+        edit_time = datetime.strptime(rd["metadata_edit_date"],
+                                      "%Y-%m-%d, %H:%M:%S")
         rd["time_since_creation"] = int((edit_time - creation_time).total_seconds())
         rd["0_sample_id"] = "%s_%s" % (rd["ark"], edit_time.isoformat())
     else:
         rd["time_since_creation"] = 0
         rd["0_sample_id"] = "%s_%s" % (rd["ark"], creation_time.isoformat())
 
-
-    #w = csv.DictWriter(sys.stdout, rd.keys())
-    #w.writeheader()
-    #w.writerow(rd)
-    if header == True:
+    if header is True:
         print "\t".join([k for k in sorted(rd)])
-        header = False       
+        header = False
     print "\t".join([str(rd[k]) for k in sorted(rd)])
